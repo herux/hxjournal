2034094a8f8f8432c9ff11f0268f3ad473a5d55f
Heru Susanto <herux@amg.co.id>
2020-08-28 09:47:27 +0700

    add java API

---
+++ api/java/src/test/java/com/herux/hxjournal/HxjournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herux.hxjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HxjournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
