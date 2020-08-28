67c59bd796455aaf5531cdadfdfa9fa425b3e568
Heru Susanto <herux@amg.co.id>
2020-08-28 12:41:33 +0700

    update

--- api/java/hxjournal/src/main/java/com/herux/repositories/JournalRepository.java
+++ api/java/hxjournal/src/main/java/com/herux/repositories/JournalRepository.java
@@ -10,19 +10,6 @@ import org.springframework.data.mongodb.repository.Query;
 public interface JournalRepository extends MongoRepository<Journal, String> {
     @Query("{ 'description' : ?0 }")
     List<Journal> findLikeDescription( String description );
-}
-
-
-// DATA DARI PUSAT : 
-
-
-// 641,1 641,2 641,3 641,4 641,1 641,3 641,5 641,4 641,1 641,6
-// | --------------------------------------------------------| 
-// INI DATA NILAI CRYPTO SEBANYAK 10 , PADA RANGE WAKTU 5 DETIK
-
-// KETIK DI BIKIN RATA RATA, MAKA 
-// 641,1 + 641,2 + 641,3 + 641,4 + 641,1 + 641,3 + 641,5 + 641,4 + 641,1 + 641,6
-// -----------------------------------------------------------------------------
-//                                     10
+}                                 10
                                                                     
                                                                     
