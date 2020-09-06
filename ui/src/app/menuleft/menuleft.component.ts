28d4026ddfecac5294805eb4140b24b5b025b0ab
herux <heruxi@gmail.com>
2020-09-06 20:25:49 +0700

    add menu class

--- ui/src/app/menuleft/menuleft.component.ts
+++ ui/src/app/menuleft/menuleft.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Menu } from '../models/menu';
 
 @Component({
   selector: 'menuleft',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menuleft.component.css']
 })
 export class MenuleftComponent implements OnInit {
+
+  menus = [
+    new Menu(1, 'Dashboard', '/dashboard'),
+    new Menu(2, 'Journal', '/journal'),
+    new Menu(3, 'COA', '/coa'),
+    new Menu(4, 'General Ledger', '/gl'),
+    new Menu(5, 'Balance Sheet', '/bs'),
+    new Menu(5, 'Income Statement', '/is'),
+  ];
 
   constructor() { }
 
---
+++ ui/src/app/models/menu.ts
@@ -0,0 +1,6 @@
+export class Menu {
+    constructor(
+        public id: number,
+        public name: string,
+        public routerLink: string) {}
+}
